0ce656ffd7150207872209fc9b05abdc041585f8
vis <48868398+vis2x@users.noreply.github.com>
2023-01-31 20:30:30 +0000

    Downgraded React and fixed issue with styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react"
-import Sidebar from "./components/Sidebar"
-import Editor from "./components/Editor"
-import { data } from "./data"
-import Split from "react-split"
-import {nanoid} from "nanoid"
+import React from "react";
+import Sidebar from "./components/Sidebar";
+import Editor from "./components/Editor";
+import { data } from "./data";
+import Split from "react-split";
+import { nanoid } from "nanoid";
+import "react-mde/lib/styles/css/react-mde-all.css";
 
 export default function App() {
 
